5127b78baf95a2b832996068366f3db2aa499a4f
buseilguz <buse.ilguz@gmail.com>
2021-01-23 17:39:41 +0300

    Add project files.

---
+++ MyDictionary2/MyDictionaryAdd.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary2
+{
+    public class MyDictionaryAdd<Keys,Values>
+    {
+        Keys[] keysArray;
+        Values[] valuesArray;
+        public MyDictionaryAdd()
+        {
+            keysArray = new Keys[0];
+            valuesArray = new Values[0];
+        }
+
+        public void Add(Keys key,Values value)
+        {
+            Keys[] tempKeyArray = keysArray;
+            Values[] tempValueArray = valuesArray;
+            keysArray = new Keys[keysArray.Length + 1];
+            valuesArray = new Values[valuesArray.Length + 1];
+
+            for (int i =  0; i < tempKeyArray.Length; i++)
+            {
+                keysArray[i] = tempKeyArray[i];
+                valuesArray[i] = tempValueArray[i];
+
+            }
+            keysArray[keysArray.Length - 1] = key;
+            valuesArray[valuesArray.Length - 1] = value;
+
+
+
+
+        }
+
+        public int howManyKey
+        {
+            get { return keysArray.Length; }
+        }
+        public int howManyValue
+        {
+            get { return valuesArray.Length; }
+        }
+        public Keys[] key
+        {
+            get { return keysArray; }
+        }
+
+        public Values[] value
+        {
+            get { return valuesArray; }
+        }
+    }
+}
---
+++ MyDictionary2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MyDictionary2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionaryAdd<string, int> nameId = new MyDictionaryAdd<string, int>();
+            Console.WriteLine("İsim giriniz: ");
+            string isim = Console.ReadLine();
+            Console.WriteLine("Girdiğiniz ismin ID numarasını giriniz");
+            int ID = Convert.ToInt32(Console.ReadLine());
+            nameId.Add(isim, ID);
+
+            Console.WriteLine("İsim giriniz: ");
+             isim = Console.ReadLine();
+            Console.WriteLine("Girdiğiniz ismin ID numarasını giriniz");
+            ID = Convert.ToInt32(Console.ReadLine());
+            nameId.Add(isim, ID);
+
+            Console.WriteLine("İsim giriniz: ");
+            isim = Console.ReadLine();
+            Console.WriteLine("Girdiğiniz ismin ID numarasını giriniz");
+            ID = Convert.ToInt32(Console.ReadLine());
+            nameId.Add(isim, ID);
+
+
+            Console.WriteLine("Dictionary içinde {0} key {1} value bulunuyor.", nameId.howManyKey, nameId.howManyValue);
+
+            Console.WriteLine("\n\n----------------Key----->Value\n----------------");
+            for (int i = 0; i < nameId.howManyKey; i++)
+            {
+                Console.WriteLine("İsim : {0}------------ID : {1}",nameId.key[i],nameId.value[i]);
+            }
+            Console.WriteLine("----------------\n");
+        }
+    }
+}
